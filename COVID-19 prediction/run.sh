724c62c2ec4644174b371245d38818b2d93ebfdc
Rahul Mullaguru <mullakurub1999@gmail.com>
2020-09-18 22:10:30 +0530

    major changes

--- COVID-19 prediction/run.sh
+++
@@ -1,9 +0,0 @@
-python3 -m pip install -r requirements.txt
-
-
-while :
-do
-	wget -O owid-covid-data.csv https://covid.ourworldindata.org/data/owid-covid-data.csv
-	python3 plots.py
-	sleep 1d
-done
